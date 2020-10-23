b19719098950a036721bb1a14ad2bf5e476e467a
David Bzura <bzuradavid@gmail.com>
2020-10-23 16:28:06 -0300

    opacity support

--- src/VueGoogleHeatmap.vue
+++ src/VueGoogleHeatmap.vue
@@ -1,57 +1,60 @@
 <template>
-  <div ref="map"
-       :style="`width: ${mapWidth}; height: ${mapHeight}`"></div>
+  <div ref="map" :style="`width: ${mapWidth}; height: ${mapHeight}`"></div>
 </template>
 
 <script>
-import { loaded } from './loader';
+import { loaded } from "./loader";
 
 export default {
-  name: 'vue-google-heatmap',
+  name: "vue-google-heatmap",
   props: {
     lat: {
       type: Number,
-      default: () => 37.775
+      default: () => 37.775,
     },
     lng: {
       type: Number,
-      default: () => -122.434
+      default: () => -122.434,
     },
     initialZoom: {
       type: Number,
-      default: () => 13
+      default: () => 13,
     },
     mapType: {
       type: String,
-      default: () => 'roadmap'
+      default: () => "roadmap",
     },
     points: {
       type: Array,
-      required: true
+      required: true,
     },
     width: {
       type: [String, Number],
-      default: () => '100%'
+      default: () => "100%",
     },
     height: {
       type: [String, Number],
-      default: () => '100%'
+      default: () => "100%",
     },
     radius: {
       type: Number,
-      default: () => 27
-    }
+      default: () => 27,
+    },
+    opacity: {
+      type: Number,
+      default: () => 0.8,
+    },
   },
   computed: {
     mapWidth() {
-      if (typeof this.width === 'string') {
+      if (typeof this.width === "string") {
         return this.width;
       } else {
         return `${this.width}px`;
       }
     },
     mapHeight() {
-      if (typeof this.height === 'string') {
+      if (typeof this.height === "string") {
         return this.height;
       } else {
         return `${this.height}px`;
@@ -59,9 +62,9 @@ export default {
     },
     heatmapPoints() {
       return this.points.map(
-        point => new google.maps.LatLng(point.lat, point.lng)
+        (point) => new google.maps.LatLng(point.lat, point.lng)
       );
-    }
+    },
   },
   created() {
     return loaded.then(() => {
@@ -70,20 +73,18 @@ export default {
       this.$mapObject = new google.maps.Map(mapElement, {
         zoom: this.initialZoom,
         center: { lat: this.lat, lng: this.lng },
-        mapTypeId: this.mapType
+        mapTypeId: this.mapType,
       });
 
       this.$heatmap = new google.maps.visualization.HeatmapLayer({
         data: this.heatmapPoints,
         map: this.$mapObject,
-        radius: this.radius
+        radius: this.radius,
+        opacity: this.opacity,
       });
 
       this.$heatmap.setMap(this.$mapObject);
     });
-  }
+  },
 };
 </script>
-
-
-
